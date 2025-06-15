03ca1cbf5e2cb8bf6fbe0006de470c262195c8cd
Pathum Udayanga <146370011+PathumUdayanga2002@users.noreply.github.com>
2025-06-16 00:38:55 +0530

    add new function

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -12,6 +12,9 @@ app.get("/ready", function (req, res) {
   res.send('{ "response": " Great!, It works!" }');
 });
 
+app.get("/", function (req, res) {
+  res.send('{ "response": "service is ready" }');
+});
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000, function () {
   console.log("App listening on port 3000!");
